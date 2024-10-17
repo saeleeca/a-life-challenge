2778a75baf7d5c528cdbc6d7b7f470128257974a
lisa fink <lisadfink@gmail.com>
2024-10-16 23:06:42 -0400

    Create basic world, organism and genome classes

---
+++ genome.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+class CreatureType(Enum):
+    PASSIVE = 1
+    HERBIVORE = 2
+    CARNIVORE = 3
+
+class Genome:
+    def __init__(self, color: (int, int, int), creature_type: CreatureType,
+                 max_energy: int, can_move: bool):
+        self._color: (int, int, int) = color
+        self._creature_type: CreatureType = creature_type
+        self._max_energy: int = max_energy
+        self._can_move: bool = can_move
+
+    def get_color(self) -> (int, int, int):
+        return self._color
+
+    def get_creature_type(self) -> CreatureType:
+        return self._creature_type
+
+    def get_max_energy(self) -> int:
+        return self._max_energy
+
+    def get_can_move(self) -> bool:
+        return self._can_move
+
+    def reproduce(self) -> 'Genome':
+        return self.__class__(self._color, self._creature_type,
+                              self._max_energy, self._can_move)
---
+++ organism.py
@@ -0,0 +1,38 @@
+class Organism:
+    def __init__(self, genome, row: int, col: int):
+        self._genome = genome
+        self._energy: int = self._genome.get_max_energy()
+        self._row: int = row
+        self._col: int = col
+
+    def move(self, row: int, col: int, world) -> None:
+        """Updates the Organisms row and col"""
+        world.move(self._row, self._col, row, col)
+        self._energy -= 1
+        self._row = row
+        self._col = col
+
+    def get_genome(self):
+        return self._genome
+
+    def get_energy(self) -> int:
+        return self._energy
+
+    def get_location(self) -> (int, int):
+        """Returns a tuple with the location (row, col)"""
+        return self._row, self._col
+
+    def eat(self, food: 'Organism', world) -> None:
+        """Consumes the food"""
+        # This might work for herbivore/carnivore but not passive
+        row, col = food.get_location()
+        self._energy += food.get_energy()
+        world.kill_organism(row, col)
+
+    def reproduce(self, row: int, col: int) -> 'Organism':
+        """Reproduces and returns the offspring"""
+        return self.__class__(self._genome.reproduce(), row, col)
+
+    def choose_action(self, world):
+        """Depends on type of Organism"""
+        pass
